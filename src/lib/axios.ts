30483508c904e14000eeef8c15cb2f0c800bce0b
Abdullah <abdullah.khalil123321@gmail.com>
2025-04-23 21:56:13 +0500

    test

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://chase-bank-api.vercel.app/api", // Correct format
-  //   baseURL: "http://localhost:5000/api",
-
   headers: {
     "Content-Type": "application/json",
   },
